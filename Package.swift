f64bd09bc9729ea554d074c47e0f2e9bfd268e78
Oleg Lutsenko <oleg.lutsenko@gmail.com>
2017-08-16 21:39:21 +0200

    Update dependency

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "OpenCloudKit",
     dependencies: [
         .Package(url: "https://github.com/vapor/clibressl.git", majorVersion: 1),
-        .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", majorVersion: 0),
+        .Package(url: "https://github.com/olutsenko/CryptoSwift.git", majorVersion: 0),
         .Package(url:  "https://github.com/DanToml/Jay.git", majorVersion: 1)
         ]
 )

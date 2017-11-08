7df64a9c3b105566f299fb97bcb35159bef1e935
Oleg Lutsenko <oleg.lutsenko@gmail.com>
2017-11-08 21:57:51 +0100

    Add logging

--- Sources/CKServerRequestAuth.swift
+++ Sources/CKServerRequestAuth.swift
@@ -71,7 +71,7 @@ struct CKServerRequestAuth {
     }
     
     static func rawPayload(withRequestDate requestDate: String, requestBody: NSData, urlSubpath: String) -> String {
-        
+        CloudKit.debugPrint("requestBody: \(String(data: requestBody as Data, encoding: .utf8) ?? "")")
         let bodyHash = requestBody.bridge().sha256()
         let hashedBody = bodyHash.base64EncodedString(options: [])
         return "\(requestDate):\(hashedBody):\(urlSubpath)"
@@ -80,12 +80,18 @@ struct CKServerRequestAuth {
     static func signature(requestDate: String, requestBody: NSData, urlSubpath: String, privateKeyPath: String) -> String? {
         
         let rawPayloadString = rawPayload(withRequestDate: requestDate, requestBody: requestBody, urlSubpath: urlSubpath)
+        CloudKit.debugPrint("rawPayloadString: \(rawPayloadString)")
       
         let requestData = rawPayloadString.data(using: String.Encoding.utf8)!
        
-        
-        let signedData = sign(data: NSData(data: requestData), privateKeyPath: privateKeyPath)
-        
+        CloudKit.debugPrint("privateKeyPath: \(privateKeyPath)")
+        let data = NSData(data: requestData)
+        CloudKit.debugPrint("data to sign: \(String(data: data as Data, encoding: .utf8) ?? "")")
+        let signedData = sign(data: data, privateKeyPath: privateKeyPath)
+        if let signedData = signedData {
+            CloudKit.debugPrint("signedData: \(String(data: signedData as Data, encoding: .utf8) ?? "")")
+        }
+
         return signedData?.base64EncodedString(options: [])
     }
     
